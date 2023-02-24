9bb265fca886249f24dc401806ed07aaa9e71946
danakoshen <114384592+danakoshen@users.noreply.github.com>
2023-02-24 13:34:54 +0900

    Update conv_lstm_model.py

--- conv_lstm_model.py
+++ conv_lstm_model.py
@@ -63,31 +63,13 @@ def generator_model():
 
     inputs  = Input((100,))
     fc1 = Dense(input_dim = 100, units = 32 * t * int(GENERATE_SQUARE/4) * int(GENERATE_SQUARE/4))(inputs)
- #   fc1 = BatchNormalization(momentum=0.8)(fc1)
-#    fc1 = LeakyReLU(0.2)(fc1) kernel_size = (4,4)
     fc2 = Reshape((t, int(GENERATE_SQUARE/4), int(GENERATE_SQUARE/4), 32), input_shape=(32 * t * int(GENERATE_SQUARE/4) * int(GENERATE_SQUARE/4),))(fc1)
-
- #   up1 = Conv3DTranspose(1, (1,3,3), strides = (1,2,2), padding='same')(fc2)
-#    up1 = TimeDistributed(UpSampling2D())(fc2)
-    up1 = TimeDistributed(Conv2D(128, (5,5), strides = (2,2), padding='same'))(fc2)  #reconsider for the next test
-#    conv1 = ConvLSTM2D(64, (3,3), strides=(1,1), padding='same', return_sequences=True)(up1) #2_kernel
+    up1 = TimeDistributed(Conv2D(128, (5,5), strides = (2,2), padding='same'))(fc2)
     conv1 = BatchNormalization(momentum=0.8)(up1)
     conv1 = ReLU()(conv1)
     conv1 = ConvLSTM2D(128, (3, 3), strides=(1, 1), padding='same', return_sequences=True)(conv1)
-#    conv1 = Dropout(0.5)(conv1)
-
-#    up2 = Conv3DTranspose(1, (1,4,4), strides = (1,2,2), padding='same')(conv1)
-#    up2 = TimeDistributed(UpSampling2D())(conv1)
-#    up2 = UpSampling3D(size = (1,2,2))(conv1)
-
     conv2 = ConvLSTM2D(64, (3,3), strides=(1,1), padding='same', return_sequences=True)(conv1) #3_kernel
-#    conv2 = BatchNormalization(momentum=0.8)(conv2)
-#    conv2 = ReLU()(conv2)
-#    conv2 = Dropout(0.5)(conv2)
     conv3 = ConvLSTM2D(48, (3,3), strides=(1,1), padding='same', return_sequences=True)(conv2) #5_kernel
-#    conv3 = ReLU()(conv3)
-#    conv3 = Dropout(0.5)(conv3)
-#    up1 = Conv3DTranspose(1, (1,4,4), strides = (1,2,2), padding='same')(conv3)
     conv3 = TimeDistributed(Conv2DTranspose(128, (5,5), strides = (2,2), padding='same'))(conv3)
 
     conv3 = BatchNormalization(momentum=0.8)(conv3)
@@ -104,29 +86,15 @@ def generator_model():
 def discriminator_model():
 
     inputs = Input((t, GENERATE_SQUARE, GENERATE_SQUARE, 1))
-    #discriminator.add(GaussianNoise(0.1, input_shape=(t, GENERATE_SQUARE, GENERATE_SQUARE, 1)))
-    #noise1 = GaussianNoise(0.1)(inputs)
     conv1 = ConvLSTM2D(128, (3,3), strides=(2,2), padding='same', return_sequences=True)(inputs) #16
-#    conv1 = LeakyReLU(0.2)(conv1)
     conv1 = Dropout(0.25)(conv1)
-    conv2 = ConvLSTM2D(64, (3,3), strides=(2,2), padding='same', return_sequences=True)(conv1) #32
-#    conv2 = BatchNormalization(momentum=0.8)(conv2)
-#    conv2 = LeakyReLU(0.2)(conv2)
     conv2 = Dropout(0.25)(conv2)
-#    conv4 = ConvLSTM2D(48, (3,3), strides=(2,2), padding='same', return_sequences=True)(conv2) #64
-#    conv4 = BatchNormalization(momentum=0.8)(conv4)
-#    conv4 = LeakyReLU(0.2)(conv4)
-#    conv4 = Dropout(0.25)(conv4)
     conv3 = ConvLSTM2D(48, (3,3), strides=(1,1), padding='same', return_sequences=True)(conv2) #128
     conv3 = BatchNormalization(momentum=0.8)(conv3)
     conv3 = ReLU()(conv3)
-#    conv3 = LeakyReLU(0.2)(conv3)
-#    conv3 = Dropout(0.5)(conv3)
     up1 = TimeDistributed(Conv2D(32, (5, 5), strides=(2, 2), padding='same'))(conv3)
     fc1 = Flatten()(up1)
-#    fc1 = Dense(1)(fc1)
     outputs = Dense(1)(fc1)
-#    outputs = Activation('tanh')(fc1)
 
     model = Model(inputs = [inputs], outputs = [outputs])
     return model
@@ -139,7 +107,6 @@ def generator_containing_discriminator(g, d):
     x = g(ganInput)
     ganOutput = d(x)
     gan = Model(inputs=ganInput, outputs=ganOutput)
-    # gan.compile(loss='binary_crossentropy', optimizer='adam')
     return gan
 
 
@@ -240,7 +207,6 @@ def train(BATCH_SIZE, X_train, n_epoch):
     d_on_g = generator_containing_discriminator(g, d)
     d_optim = RMSprop(lr=0.00005) #0.0003
     g_optim = RMSprop(lr=0.00005) #0.0002
-#    g.compile(loss='mse', optimizer=g_optim)
     d_on_g.compile(loss=wasserstein_loss, optimizer=g_optim)
     d.trainable = True
     d.compile(loss=wasserstein_loss, optimizer=d_optim)
@@ -257,29 +223,6 @@ def train(BATCH_SIZE, X_train, n_epoch):
         progress_bar = Progbar(target=n_iter)
 
         for index in range(n_iter):
-            # create random noise -> U(0,1) 10 latent vectors
-            # noise = np.random.normal(0, 1, size=(BATCH_SIZE, 100))
-
-            # load real data & generate fake data
-            # image_batch = X_train[index * BATCH_SIZE:(index + 1) * BATCH_SIZE]
-            # generated_images = g.predict(noise, verbose=0)
-
-            # visualize training results
-#            if index % 20 == 0:
-#                image = combine_images(generated_images)
-#                image = image * 127.5 + 127.5
-#                cv2.imwrite('./result/' + str(epoch) + "_" + str(index) + ".png", image)
-
-            # attach label for training discriminator
-            #X = np.concatenate((image_batch, generated_images))
-            #y = np.array([1] * BATCH_SIZE + [0] * BATCH_SIZE)
-
-            # training discriminator
-            #d_loss = d.train_on_batch(X, y)
-            # d_loss_real = d.train_on_batch(image_batch, valid)
-            # d_loss_fake = d.train_on_batch(generated_images, fake)
-            # d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
-
             for _ in range(n_critic):
 
                 # ---------------------
@@ -308,11 +251,6 @@ def train(BATCH_SIZE, X_train, n_epoch):
                     weights = l.get_weights()
                     weights = [np.clip(w, -clip_value, clip_value) for w in weights]
                     l.set_weights(weights)
-
-            # for l in d.layers:
-            #     weights = l.get_weights()
-            #     weights = [np.clip(w, -clip_value, clip_value) for w in weights]
-            #     l.set_weights(weights)
 
             # training generator
             d.trainable = False
@@ -406,49 +344,10 @@ if args.mode == 'train':
     X_train = hickle.load('X_train.hkl')
     Model_d, Model_g = train(16, X_train, n_epoch = args.n_epoch)
 
-### 1. train generator & discriminator
-# if args.mode == 'train':
-#     X_train = np.load('training_data_28_28.npy', allow_pickle=True)
-#     print('Original train shape: ' + str(X_train.shape))
-#     X_train = X_train[:, :, :, None]
-#     if args.class_n == '1':
-#         X_train_1 = np.load('training_data_1_28_28.npy', allow_pickle=True)
-#         print('Class 1 train shape: ' + str(X_train_1.shape))
-#         X_train_1 = X_train_1[:, :, :, None]
-#         Model_d, Model_g = train(64, X_train_1, class_n='1', n_epoch=args.n_epoch)
-#     elif args.class_n == '2':
-#         X_train_2 = np.load('training_data_2_28_28.npy', allow_pickle=True)
-#         print('Class 2 train shape: ' + str(X_train_2.shape))
-#         X_train_2 = X_train_2[:, :, :, None]
-#         Model_d, Model_g = train(64, X_train_2, class_n='2', n_epoch=args.n_epoch)
-#     else:
-#         Model_d, Model_g = train(64, X_train, class_n='0', n_epoch=args.n_epoch)
 X_test = hickle.load('X_test.hkl')
 y_test = np.load('Test_ped1.npy', allow_pickle=True)
 
-### 2. test generator
-# generated_img = generate(25)
-# img = combine_images(generated_img)
-# img = (img * 127.5) + 127.5
-# img = img.astype(np.uint8)
-# img = cv2.resize(img, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
 
-### opencv view
-# cv2.namedWindow('generated', 0)
-# cv2.resizeWindow('generated', 256, 256)
-# cv2.imshow('generated', img)
-# cv2.imwrite('result_latent_10/generator.png', img)
-# cv2.waitKey()
-
-### plt view
-# plt.figure(num=0, figsize=(4, 4))
-# plt.title('trained generator')
-# plt.imshow(img, cmap=plt.cm.gray)
-# plt.show()
-
-# exit()
-
-### 3. other class anomaly detection
 
 
 ano_score = None
@@ -475,20 +374,11 @@ def anomaly_detection(model, test_img, g=None, d=None):
 
 
 
-### compute anomaly score - sample from test set
-# test_img = X_test_original[y_test==1][30]
-
-### compute anomaly score - sample from strange image
-# test_img = X_test_original[y_test==0][30]
-
-### compute anomaly score - sample from strange image
-# test_img = globals()['X_test_'+class_n][img_idx]
 model = anomaly_detector(g=None, d=None)
 
 scores = np.ndarray((len(X_test)), np.float32)
 model = anomaly_detector()
 for i in range(len(X_test)):
-#for i in range(500,511):
     score, orig, sim, res = anomaly_detection(model, X_test[i], t)
     scores[i] = score
     if(i+1)%2==0:
@@ -507,61 +397,3 @@ from PIL import Image
 scores_1 = np.squeeze(np.array(Image.fromarray(np.expand_dims(scores, 1).astype(np.float32)).resize((1,len(y_test)))))
 
 np.save(OUTPUT_PATH + '/scores.npy',scores_1)
-# test_img = np.random.normal(-1,1, (28,28,1))
-#
-# start = cv2.getTickCount()
-# score, qurey, pred, diff = anomaly_detection(test_img, class_n)
-# time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
-# print ('%d : done'%(img_idx), '%.2f'%score, '%.2fms'%time)
-## cv2.imwrite('./qurey.png', qurey)
-## cv2.imwrite('./pred.png', pred)
-## cv2.imwrite('./diff.png', diff)
-#
-### matplot view
-# plt.figure(1, figsize=(3, 3))
-# plt.title('query image')
-# plt.imshow(qurey.reshape(28,28), cmap=plt.cm.gray)
-#
-# print("anomaly score : ", score)
-# plt.figure(2, figsize=(3, 3))
-# plt.title('generated similar image')
-# plt.imshow(pred.reshape(28,28), cmap=plt.cm.gray)
-#
-# plt.figure(3, figsize=(3, 3))
-# plt.title('anomaly detection')
-# plt.imshow(cv2.cvtColor(diff,cv2.COLOR_BGR2RGB))
-# plt.show()
-
-
-### 4. tsne feature view
-
-### t-SNE embedding
-### generating anomaly image for test (radom noise image)
-
-# from sklearn.manifold import TSNE
-#
-# random_image = np.random.normal(0, 1, (100, 28, 28, 1))
-# print("random noise image")
-# plt.figure(4, figsize=(2, 2))
-# plt.title('random noise image')
-# plt.imshow(random_image[0].reshape(28,28), cmap=plt.cm.gray)
-#
-## intermidieate output of discriminator
-# model = anogan.feature_extractor(class_n='1')
-# feature_map_of_random = model.predict(random_image, verbose=1)
-# feature_map_of_minist = model.predict(X_test[:len(X_test)], verbose=1)
-# feature_map_of_minist_1 = model.predict(X_test[:len(X_test)], verbose=1)
-#
-## t-SNE for visulization
-# output = np.concatenate((feature_map_of_random, feature_map_of_minist, feature_map_of_minist_1))
-# output = output.reshape(output.shape[0], -1)
-# anomaly_flag = np.array([1]*100+ [0]*300)
-#
-# X_embedded = TSNE(n_components=2).fit_transform(output)
-# plt.figure(5)
-# plt.title("t-SNE embedding on the feature representation")
-# plt.scatter(X_embedded[:100,0], X_embedded[:100,1], label='random noise(anomaly)')
-# plt.scatter(X_embedded[100:400,0], X_embedded[100:400,1], label='mnist(anomaly)')
-# plt.scatter(X_embedded[400:,0], X_embedded[400:,1], label='mnist(normal)')
-# plt.legend()
-# plt.show()
